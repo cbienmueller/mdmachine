17cd9923d5ce9e18dc699587505d5fe436417bf6
Christian Bienmüller <christian@bienmueller.de>
2026-05-26 14:49:15 +0200

    Systax Theme solarizeddark

--- mdmachine.py
+++ mdmachine.py
@@ -164,6 +164,6 @@ if __name__ == '__main__':
     # ### START ### #
     # ############# #
 
-    print('mdmachine Version 1.0.7 von 2026-02-26')
+    print('mdmachine Version 1.0.7b von 2026-05-26')
     start_your_engines()
         
